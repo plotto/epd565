5c9b9fe31b53a0e7952d77d0c8efb02962307305
plotto <plotto@users.noreply.github.com>
2020-09-10 18:34:22 +0000

    Update bmp.py

--- bmp.py
+++ bmp.py
@@ -13,6 +13,10 @@ parser.add_argument('-o', help='output file')
 parser.add_argument('-r', type=float, help='red intensity scalar')
 parser.add_argument('-g', type=float, help='green intensity scalar')
 parser.add_argument('-b', type=float, help='blue intensity scalar')
+parser.add_argument('--top', type=int, help='distance from top for crop')
+parser.add_argument('--bottom', type=int, help='distance from top for crop')
+parser.add_argument('--left', type=int, help='distance from left for crop')
+parser.add_argument('--right', type=int, help='distance from left for crop')
 parser.add_argument('--lightness', type=float, help='lighten/darken scalar')
 parser.add_argument('--saturation', type=float, help='color saturation scalar')
 parser.add_argument('-p', help='serial port', nargs='?',const="/dev/cu.SLAB_USBtoUART")
@@ -21,6 +25,8 @@ parser.add_argument('-c', help='how many times to clear-cycle the eInk screen', 
 parser.add_argument('--showEinkPalette', help='show eink paletted bmp', action="store_true")
 args = vars(parser.parse_args())
 
+assert((args['left'] != None and args['right'] == None) or (args['left'] == None and args['right'] != None) or (args['left'] == None and args['right'] == None))
+assert((args['top'] != None and args['bottom'] == None) or (args['top'] == None and args['bottom'] != None) or (args['top'] == None and args['bottom'] == None))
 # this is the palette with the RGB values needed to drive the eink display
 finalPaletteByteArray = bytearray([0x00, 0x00, 0xFF, 0x00, 0x00, 0xFF, 0x00,0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x80, 0xFF,0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0xFF,0x00] * 32)
 
@@ -28,7 +34,9 @@ finalPaletteByteArray = bytearray([0x00, 0x00, 0xFF, 0x00, 0x00, 0xFF, 0x00,0x00
 paletteImage = Image.new('P', (1, 1))
 #paletteImage.putpalette([int(x) for x in [0x78, 0x40, 0x43, 0x32, 0x5C, 0x3D, 0x35, 0x3F, 0x56, 0xAB, 0x9E, 0x54, 0xA4, 0x75, 0x4D, 0x30, 0x30, 0x30, 0xAF, 0xAF, 0xAF, 0xAF, 0xAF, 0xAF]] * 32)
 
-paletteImage.putpalette([int(x) for x in [0x92, 0x5a, 0x5d, 0x50, 0x73, 0x54, 0x52, 0x5b, 0x74, 0xad, 0xa4, 0x68, 0xa5, 0x7e, 0x61, 0x20, 0x20, 0x20, 0xb1, 0xb1, 0xb2, 0xb1, 0xb1, 0xb2]] * 32)
+#paletteImage.putpalette([int(x) for x in [0x92, 0x5a, 0x5d, 0x50, 0x73, 0x54, 0x52, 0x5b, 0x74, 0xad, 0xa4, 0x68, 0xa5, 0x7e, 0x61, 0x20, 0x20, 0x20, 0xb1, 0xb1, 0xb2, 0xb1, 0xb1, 0xb2]] * 32)
+
+paletteImage.putpalette([int(x) for x in [0x92, 0x5a, 0x5d, 0x50, 0x73, 0x54, 0x52, 0x5b, 0x74, 0xa0, 0xa0, 0x60, 0xa5, 0x7e, 0x61, 0x20, 0x20, 0x20, 0xb1, 0xb1, 0xb2, 0xb1, 0xb1, 0xb2]] * 32)
 
 # target image size
 (targetwidth, targetheight) = (600.0, 448.0)
@@ -37,11 +45,19 @@ paletteImage.putpalette([int(x) for x in [0x92, 0x5a, 0x5d, 0x50, 0x73, 0x54, 0x
 im = Image.open(args['i'])
 im = im.convert("RGB")
 
+#crop arguments
+
+
 # rotate to horizontal
 (width, height) = im.size
 if width < height:
   im = im.transpose(Image.ROTATE_90)
   (width, height) = im.size
+  if args['top'] != None or args['left'] != None:
+    temp = args['left']
+    args['left'] = args['top']
+    args['top'] = temp
+ 
 
 # resize and crop based on relative aspect ratio to make
 #   sure there is no empty space
@@ -50,12 +66,40 @@ if width < height:
 if width/height < targetwidth/targetheight:
   percent = targetwidth / float(width)
   im = im.resize((int(width * percent),int(height * percent)))
-  im = im.crop((0,int((height * percent - targetheight) / 2),600,int((height * percent - targetheight) / 2) + 448))
+  left = 0
+  if args['top'] != None:
+    top = args['top']
+  elif args['bottom'] != None:
+    top = int(height * percent) - args['bottom'] - 448
+  else:
+    top = int((height * percent - targetheight) / 2)
+  right = 600
+  if args['top'] != None:
+    bottom = args['top'] + 448
+  elif args['bottom'] != None:
+    bottom = int(height * percent) - args['bottom']
+  else:
+    bottom = int((height * percent - targetheight) / 2) + 448
+  im = im.crop((left,top,right,bottom))
 # if it's too wide, fit on height and crop width
 else:
   percent = targetheight / float(height)
   im = im.resize((int(width * percent),int(height * percent)))
-  im = im.crop((int((width * percent - targetwidth) / 2),0,int((width * percent - targetwidth) / 2) + 600,448))
+  if args['left'] != None:
+    left = args['left']
+  elif args['right'] != None:
+    left = int(width * percent) - args['right'] - 600
+  else:
+    left = int((width * percent - targetwidth) / 2)
+  top = 0
+  if args['left'] != None:
+    right = args['left'] + 600
+  elif args['right'] != None:
+    right = int(width * percent) - args['right']
+  else:
+    right = int((width * percent - targetwidth) / 2) + 600
+  bottom = 448
+  im = im.crop((left,top,right,bottom))
 
 # process lightness/darkness options
 if args['lightness']:
